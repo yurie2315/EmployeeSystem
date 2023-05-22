e14fff147439ec02defcc24a1d8bc214a8bacb27
githubrnr <githubrnr@gmail.com>
2023-05-22 17:28:17 +0800

    first commit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package net.javaguides.springboot.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+
+    private static final long serialVersionUID = 5310524671813718793L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ springboot-backend/src/test/java/net/javaguides/springboot/controller/EmployeeControllerTest.java
@@ -0,0 +1,7 @@
+package net.javaguides.springboot.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+}
